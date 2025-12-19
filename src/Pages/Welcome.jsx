b395ce3491feab3f7508274adf3eb98533f63a53
LittleStar21 <leonardian14@gmail.com>
2025-12-19 21:21:22 +0800

    Add welcome page

--- src/Pages/Welcome.jsx
+++ src/Pages/Welcome.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import "./Welcome.css";
 
+import snoopyImg from "./assets/hello.png";
+
 const Welcome = ({ onNext }) => {
   const [input, setInput] = useState("");
   const correctName = "Gamin";
@@ -16,75 +18,79 @@ const Welcome = ({ onNext }) => {
   }
 
   return (
-    <div className="welcome-background">
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{
-          opacity: 1,
-          y: 0,
-          transition: { duration: 2, ease: "easeOut" },
-        }}
-      >
-        <motion.div
-          className="welcome-title"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: { delay: 1, duration: 0.8 },
-          }}
-        >
-          Welcome!
-        </motion.div>
+    <div>
+      <div className="welcome-topbar"></div>
+      <div className="welcome-background">
+        <div className="welcome-container">
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              transition: { duration: 2, ease: "easeOut" },
+            }}
+          >
+            <motion.div
+              className="welcome-title"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { delay: 1, duration: 0.8 },
+              }}
+            >
+              Hello!
+            </motion.div>
 
-        <motion.div
-          className="welcome-text"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: { delay: 2, duration: 0.8 },
-          }}
-        >
-          Hello! Please input your name
-        </motion.div>
+            <motion.div
+              className="welcome-text"
+              initial={{ opacity: 0, y: 50 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { delay: 2, duration: 0.8 },
+              }}
+            >
+              Please enter your name
+            </motion.div>
 
-        <motion.input
-          type="text"
-          className="w-full border rounded-xl p-3 text-gray-700 mb-2"
-          placeholder="Your name"
-          value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-            setError(false);
-          }}
-          initial={{ opacity: 0, y: 50 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: { delay: 3, duration: 0.8 },
-          }}
-        ></motion.input>
+            <motion.input
+              type="text"
+              placeholder="Your name"
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value);
+                setError(false);
+              }}
+              initial={{ opacity: 0, y: 50 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { delay: 2, duration: 0.8 },
+              }}
+            ></motion.input>
 
-        {error && (
-          <p className="text-red-500 text-sm mb-2 text-center">
-            Incorrect Name
-          </p>
-        )}
+            {error && <div>Incorrect Name</div>}
 
-        <motion.button
-          onClick={handleSubmit}
-          className="w-full bg-pink-500 text-white py-3 rounded-xl mt-4 hover:bg-pink-600 transition-all"
-          initial={{ opacity: 0, y: 50 }}
-          animate={{
-            opacity: 1,
-            y: 0,
-            transition: { delay: 3, duration: 0.8 },
-          }}
-        >
-          Next page
-        </motion.button>
-      </motion.div>
+            <motion.button
+              onClick={handleSubmit}
+              initial={{ opacity: 0, y: 50 }}
+              animate={{
+                opacity: 1,
+                y: 0,
+                transition: { delay: 2, duration: 0.8 },
+              }}
+            >
+              Next page
+            </motion.button>
+          </motion.div>
+        </div>
+        <img
+          src={snoopyImg}
+          alt="Snoopy image"
+          className="welcome-snoopy-img"
+        ></img>
+      </div>
     </div>
   );
 };
